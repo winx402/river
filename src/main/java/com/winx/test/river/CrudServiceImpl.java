943f5b2ac5f1a53e6369fb4df804551944e5256d
wangwenxiang <wangwenxiang1993@gmail.com>
2017-10-24 23:11:02 +0800

    update

--- src/main/java/com/winx/test/river/CrudServiceImpl.java
+++ src/main/java/com/winx/test/river/CrudServiceImpl.java
@@ -5,10 +5,7 @@ import com.winx.exception.RateLimitTimeOutException;
 import com.winx.impl.aop.Aop;
 import com.winx.impl.cache.Cache;
 import com.winx.impl.limit.RateLimit;
-import com.winx.impl.pool.AbstractCarrier;
-import com.winx.impl.pool.Carrier;
-import com.winx.impl.pool.SingleThreadExecutor;
-import com.winx.impl.pool.ThreadPool;
+import com.winx.impl.pool.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -20,11 +17,11 @@ import java.util.concurrent.TimeUnit;
 @Resource
 public class CrudServiceImpl implements com.winx.test.river.Test{
 
-    @Aop(AopTest.class)
-    public String aop(String param) throws InterruptedException {
+    @FixedThreadPool(threads = 10)
+    public Carrier<String> aop(String param) throws InterruptedException {
 //        System.out.println("aop param : " + param);
         Thread.currentThread().sleep(100);
-        return param + " success";
+        return AbstractCarrier.loading("result : " + param);
     }
 
     @Cache(maxSize = 2, timeOut = 1, timeUnit = TimeUnit.MINUTES)
@@ -33,30 +30,32 @@ public class CrudServiceImpl implements com.winx.test.river.Test{
         return param + " success";
     }
 
-    public static void main(String[] args){
-        final CrudServiceImpl proxy = ProxyFactory.getSingleProxy(CrudServiceImpl.class);
-        for (int i = 0; i < 10; i ++){
-            System.out.println(proxy.cache("cache key"));
+    public static void main(String[] args) throws InterruptedException {
+        final CrudServiceImpl[] proxy = new CrudServiceImpl[1];
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                proxy[0] = ProxyFactory.getSingleProxy(CrudServiceImpl.class);
+            }
+        });
+        thread.run();
+        thread.join();
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < 10; i++){
+            threads[i] = new Thread(new Runnable() {
+                public void run() {
+                    for (int i = 0; i < 1; i++){
+                        try {
+                            System.out.println(Thread.currentThread().getName() + ":" + proxy[0].aop("test").get());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }, "thread-"+i);
         }
-//        long l = System.currentTimeMillis();
-//        Thread[] threads = new Thread[10];
-//        for (int i = 0; i < 10; i++){
-//            threads[i] = new Thread(new Runnable() {
-//                public void run() {
-//                    for (int i = 0; i < 1; i++){
-//                        try {
-//                            System.out.println(Thread.currentThread().getName() + ":" + proxy.aop("test"));
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }
-//            }, "thread-"+i);
-//        }
-//        for (int i = 0; i < 10; i++){
-//            threads[i].start();
-//        }
-//        System.out.println("cost:" +(System.currentTimeMillis() - l));
+        for (int i = 0; i < 10; i++){
+            threads[i].start();
+        }
     }
 
     public void doIt() {
